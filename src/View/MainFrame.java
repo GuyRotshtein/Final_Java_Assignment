ee5015a84ce30911fc33d9a6873e9e1dd35c2fc3
Guy Rotshtein <100364697+GuyRotshtein@users.noreply.github.com>
2023-03-02 12:23:49 +0200

    Updated GBD's work :)

--- src/View/MainFrame.java
+++ src/View/MainFrame.java
@@ -1,5 +1,5 @@
 package View;
-
+import Model.Category;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -12,8 +12,13 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.border.Border;
 import javax.swing.JLabel;
-
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import java.util.HashMap;
 import javax.swing.JTable;
 import javax.swing.plaf.basic.BasicOptionPaneUI;
@@ -30,8 +35,10 @@ public class MainFrame extends JFrame {
 	private JButton Category;
 	private JButton ShowCategory;
 	private JButton SearchCategory;
+	private  JTable Mtable ;
 
 	private ActionListener buttonListener;
+	private JScrollPane sp;
 
 
 
@@ -41,7 +48,7 @@ public class MainFrame extends JFrame {
 		 this.getContentPane().setBackground(Color.CYAN);
 		 this.setLayout(null);
 		 this.setVisible(true);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 	}
 	class MyButtonListener implements ActionListener
 	{
@@ -54,7 +61,7 @@ public class MainFrame extends JFrame {
 					System.out.println(e.getActionCommand());
 					break;
 				case "Category":
-					System.out.println(e.getActionCommand());
+					create_table();
 					break;
 				case "ShowCategory":
 					System.out.println(e.getActionCommand());
@@ -68,7 +75,27 @@ public class MainFrame extends JFrame {
 		}
 
 	}
+	public void create_table(){
+		System.out.println("enter the function create_table");
+		String[] ColmNames = {"id" , "names"};
+		Model.Category c1 = new Category(1,"Cloth");
+		Model.Category c2 = new Category(2,"Gaz");
+		String data [][] = {{c1.toString()},{c2.toString()}};
+		System.out.println("we create data and Colm");
+		Mtable = new JTable(data , ColmNames);
+		Mtable.setBounds(30,40,200,300);
+		JScrollPane sp=new JScrollPane(Mtable);
+		this.add(sp);
+		this.setSize(300,400);
+		this.setVisible(true);
 
+
+
+
+
+
+
+	}
 	public MainFrame()
 	{
 
@@ -101,7 +128,7 @@ public class MainFrame extends JFrame {
 		this.setLayout(null);
 		this.setVisible(true);
 		System.out.println("bfa");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 
 	    
 	}
