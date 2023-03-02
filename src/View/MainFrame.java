0be8dda248168bd74ef9b323b7a34229c5f24956
Guy Rotshtein <100364697+GuyRotshtein@users.noreply.github.com>
2023-03-02 12:29:00 +0200

    Fixed View to GBD's standard
    oops v2

--- src/View/MainFrame.java
+++ src/View/MainFrame.java
@@ -29,107 +29,78 @@ public class MainFrame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JTable table;
+	private JTable expanseTable;
 	private JFrame myFrame;
-	private JButton BtCost;
+	private JButton btCost;
 	private JButton Category;
 	private JButton ShowCategory;
 	private JButton SearchCategory;
-	private  JTable Mtable ;
+	private JTable Mtable ;
 
 	private ActionListener buttonListener;
 	private JScrollPane sp;
-
-
-
-	public void open_new_gui(){
-		 System.out.println("open new ui ");
-		 this.setLayout(new FlowLayout());
-		 this.getContentPane().setBackground(Color.CYAN);
-		 this.setLayout(null);
-		 this.setVisible(true);
-		 this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
-	}
-	class MyButtonListener implements ActionListener
+	public MainFrame(String[] tableColumns, Object[][] tableData)
 	{
-		public void actionPerformed(ActionEvent e)
-		{
-			Object source = e.getSource();
-			switch (e.getActionCommand())
-			{
-				case "AddCost":
-					System.out.println(e.getActionCommand());
-					break;
-				case "Category":
-					create_table();
-					break;
-				case "ShowCategory":
-					System.out.println(e.getActionCommand());
-					break;
-				case "SearchCategory":
-					System.out.println(e.getActionCommand());
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		/* CONTENT PANE SETUP */
+		JPanel contentPanel = new JPanel();
+		BoxLayout pageLayout = new BoxLayout(contentPanel, BoxLayout.PAGE_AXIS);
+		this.setContentPane(contentPanel);
+		this.getContentPane().setLayout(pageLayout);
+		this.getContentPane().setBackground(Color.darkGray);
+//		buttonListener = new MyButtonListen+er();
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		/* BUTTON SETUP */
 
-			}
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setBackground(Color.darkGray);
+		buttonPanel.setLayout(new BoxLayout(buttonPanel ,BoxLayout.LINE_AXIS));
 
+		btCost = new JButton("Add Expanse");
+		btCost.addActionListener(buttonListener);
+		buttonPanel.add(btCost);
+		buttonPanel.add(Box.createRigidArea(new Dimension(8, 20)));
 
-		}
-
-	}
-	public void create_table(){
-		System.out.println("enter the function create_table");
-		String[] ColmNames = {"id" , "names"};
-		Model.Category c1 = new Category(1,"Cloth");
-		Model.Category c2 = new Category(2,"Gaz");
-		String data [][] = {{c1.toString()},{c2.toString()}};
-		System.out.println("we create data and Colm");
-		Mtable = new JTable(data , ColmNames);
-		Mtable.setBounds(30,40,200,300);
-		JScrollPane sp=new JScrollPane(Mtable);
-		this.add(sp);
-		this.setSize(300,400);
-		this.setVisible(true);
-
-
-
-
-
-
-
-	}
-	public MainFrame()
-	{
-
-
-		this.setLayout(new FlowLayout());
-		this.getContentPane().setBackground(Color.black);
-		buttonListener = new MyButtonListener();
-
-		BtCost = new JButton("AddCost");
-		BtCost.setBounds(0,10,100,30);
-		BtCost.addActionListener(buttonListener);
-
-		Category = new JButton("Category");
-		Category.setBounds(90,10,100,30);
+		Category = new JButton("Add Category");
 		Category.addActionListener(buttonListener);
+		buttonPanel.add(Category);
+		buttonPanel.add(Box.createRigidArea(new Dimension(8, 20)));
 
-		ShowCategory = new JButton("ShowCategory");
-		ShowCategory.setBounds(180,10,150,30);
+		ShowCategory = new JButton("Show Category");
 		ShowCategory.addActionListener(buttonListener);
+		buttonPanel.add(ShowCategory);
+		buttonPanel.add(Box.createRigidArea(new Dimension(8, 20)));
 
-		SearchCategory = new JButton("SearchCategory");
-		SearchCategory.setBounds(300,10,150,30);
+		SearchCategory = new JButton("Search Category");
 		SearchCategory.addActionListener(buttonListener);
+		buttonPanel.add(SearchCategory);
+		this.add(Box.createRigidArea(new Dimension(8, 20)));
+		this.add(buttonPanel);
+		this.add(Box.createRigidArea(new Dimension(5, 20)));
 
-		this.setSize(800, 800);
-		this.add(BtCost);
-		this.add(Category);
-		this.add(ShowCategory);
-		this.add(SearchCategory);
-		this.setLayout(null);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		/* TABLE SETUP */
+		JPanel tablePanel = new JPanel(null);
+		tablePanel.setBackground(Color.darkGray);;
+		tablePanel.setSize(new Dimension(800, 600));
+
+		expanseTable = new JTable(tableData.length+1, tableColumns.length);
+		DefaultTableModel tableModel = new DefaultTableModel(tableData, tableColumns);
+		expanseTable.setModel(tableModel);
+		expanseTable.setGridColor(Color.GRAY);
+		expanseTable.setFont(new Font("Arial",Font.BOLD, 14));
+		JScrollPane scrollPane = new JScrollPane(expanseTable);
+		scrollPane.setBounds(112, 0, 800, 300);
+		tablePanel.add(scrollPane);
+		this.add(tablePanel);
+		this.add(Box.createRigidArea(new Dimension(5, 20)));
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		/* FINAL SETUP */
+
+		this.setSize(1024, 786);
+
+		this.setLocationRelativeTo(null);
 		this.setVisible(true);
-		System.out.println("bfa");
 
-
-	    
 	}
 }
