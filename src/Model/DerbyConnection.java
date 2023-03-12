94646de78cecfd19b62a54b2e4904b00f0692e68
Guy Rotshtein <100364697+GuyRotshtein@users.noreply.github.com>
2023-03-12 09:46:12 +0200

    Added comments to the DerbyConnection file

--- src/Model/DerbyConnection.java
+++ src/Model/DerbyConnection.java
@@ -20,10 +20,13 @@ public class DerbyConnection {
     private String DBNAME = "derbyDB";
     private String[] TABLE_COLUMNS = {"ID","AMOUNT", "CURRENCY" ,"CATEGORY", "DESCRIPTION", "DATE"};
     ////////////////////////////////////////////////
-
+    // Statement ID list
     private ArrayList<Statement> statements;
-    private Statement sqlCmd;
+    // Connection to the Database
     private Connection conn = null;
+    //Default statement
+    private Statement sqlCmd;
+    /*PreparedStatement objects*/
     private PreparedStatement psCategoryInsert, psCostInsert;
     private PreparedStatement psCategoryUpdate, psCostUpdate;
     private PreparedStatement psCategoryDelete, psCostDelete;
@@ -33,11 +36,20 @@ public class DerbyConnection {
 
 //    private MainFrame gui;
 
+    /**
+     * This is the Constructor. runs all the required functions for initially creating a
+     * Derbydb connection to an embedded database.
+     *
+     */
     public DerbyConnection(){
         createConnection();
         prepareDB();
     }
 
+    /**
+     * Creating the connection to the DerbyDB embedded server. This function defines the connection properties,
+     * and creates a connection to the database using them.
+     */
     public void createConnection(){
         Properties props = new Properties(); // connection properties
         try {
@@ -54,6 +66,9 @@ public class DerbyConnection {
 
     }
 
+    /**
+     * Checking if both tables already exist, and Creating both if they don't exist already.
+     */
     public void prepareDB(){
         try {
 //            deleteTables();
@@ -65,6 +80,11 @@ public class DerbyConnection {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Delete both tables and their data
+     * @throws Exception
+     */
     public void deleteTables() throws Exception{
         sqlCmd = conn.createStatement();
         sqlCmd.execute("drop table Cost ");
@@ -76,6 +96,10 @@ public class DerbyConnection {
 
 
 /*Catagory secion*/
+
+    /**
+     * Creating the "CREATE" statement for the category table, and adding it to the existing statements.
+     */
     public void createCategories() {
         try {
             sqlCmd = conn.createStatement();
@@ -97,6 +121,9 @@ public class DerbyConnection {
         }
     }
 
+    /**
+     * Contacts the database, and returns an arraylist of all categories added to the database.
+     */
     public ArrayList<Category> getAllCategories(){
         ArrayList<Category> result = new ArrayList<>();
         try {
@@ -120,6 +147,11 @@ public class DerbyConnection {
         return result;
     }
 
+    /**
+     * Recieves a Category object, and uses its details to create a statement for inserting it into the database.
+     * the Category file gets passed through ViewModel, and gets created there.
+     * @param cat
+     */
     public void insertCategory(Category cat){
         try{
             psCategoryInsert = conn.prepareStatement(
@@ -134,16 +166,24 @@ public class DerbyConnection {
         }
     }
 
+    /**
+     * Recievs an integer representing the ID of an existing category, and searches through the
+     * database for it. Returns the found category object.
+     * @param id
+     * @return
+     */
     public Category getCategoryByID(int id){
         Category result = null;
         try {
             sqlCmd = conn.createStatement();
-
+            // creating a preparedStatement for searching for the Category by ID
             PreparedStatement getCategoryPs = conn.prepareStatement(
                     "SELECT * FROM Categories WHERE Id = (?)");
             getCategoryPs.setInt(1, id);
+            //Sending the query to the database
             ResultSet resultSet = getCategoryPs.executeQuery();
             while (resultSet.next()){
+                // receiving the Int from the 1st colum and the category String from the 2nd colum
                 result = new Category(resultSet.getInt(1), resultSet.getString(2));
             }
             resultSet.close();
@@ -154,13 +194,21 @@ public class DerbyConnection {
         }
         return result;
     }
-
+    /**
+     * Recievs a Category object representing the ID of an existing category, and searches through the
+     * database for it. Returns the found category object.
+     * @param cat
+     * @return
+     */
     public int getCategoryID(Category cat ){
         int catID = -1;
         try{
             sqlCmd = conn.createStatement();
+            // creating a preparedStatement for searching for the Category by the given category's details
             String sql = "SELECT * FROM Categories WHERE Name = '" + cat.getName() + "'";
+            //Sending the query to the database
             ResultSet result = sqlCmd.executeQuery(sql);
+            // receiving the Category object from the database
             while (result.next()) catID = result.getInt(1);
             result.close();
             sqlCmd.close();
@@ -173,7 +221,12 @@ public class DerbyConnection {
     }
 
 
-
+    /**
+     * Searches the database for a category based on its name, and returns
+     * said category if it finds one with the same name in the database.
+     * @param catName
+     * @return
+     */
     public boolean checkCategory(String catName)
     {
         boolean res;
@@ -200,6 +253,9 @@ public class DerbyConnection {
         return res;
     }
 /*cost section*/
+    /**
+     * Creating the "CREATE" statement for the Cost table, and adding it to the existing statements.
+     */
     public void createCostTable() {
         try{
             sqlCmd = conn.createStatement();
@@ -224,7 +280,9 @@ public class DerbyConnection {
         }
 
     }
-
+    /**
+     * Contacts the database, and returns an arraylist of all costs added to the database.
+     */
     public ArrayList<Cost> getAllCosts(){
         ArrayList<Cost> allCosts = new ArrayList<>();
         try {
@@ -257,12 +315,15 @@ public class DerbyConnection {
 
 
 
-    /*
-    TODO - switch insertCost to recieve a Record, use the record to input the data
+    /**
+     * Recieves a Cost object, and uses its details to create a statement for inserting it into the database.
+     * the Cost file gets passed through ViewModel, and gets created there.
+     * @param c
      */
     public void insertCost(Cost c){
         try{
             int catID = getCategoryID(c.getCategory());
+            //creating the preparedStatement
             psCostInsert = conn.prepareStatement(
                         "insert into Cost " +
                             "(costSum,currency,category, description, costDate)" +
@@ -272,7 +333,8 @@ public class DerbyConnection {
             psCostInsert.setString(2,c.getCurrency());
             psCostInsert.setInt(3,catID);
             psCostInsert.setString(4,c.getDescription());
-            psCostInsert.setDate(5,c.getDate()); // check date class
+            psCostInsert.setDate(5,c.getDate());
+            //executing the preparedStatement to insert the Cost to the database
             psCostInsert.executeUpdate();
             System.out.println("Inserted Record\t" + c);
             psCostInsert.close();
@@ -281,6 +343,11 @@ public class DerbyConnection {
             sqle.printStackTrace();
         }
     }
+
+    /**
+     * Returns and prints all data of Costs stored in the database.
+     * @param s - given statement by model.
+     */
     private void getCostData(Statement s)
     {
         try
@@ -306,15 +373,21 @@ public class DerbyConnection {
 
     }
 
+    /**
+     * Searches the database and returns an array of all costs that were made in the same year as a given parameter.
+     *
+     * @param year - parameter of search query, passed through ViewModel.
+     * @return - returns an array of all costs found.
+     */
     public ArrayList<Cost> searchCosts(int year){
         String searchYear = String.valueOf(year);
         ArrayList<Cost> searchCosts = new ArrayList<>();
         try {
             sqlCmd = conn.createStatement();
-            //System.out.println("Printing all categories");
+            //Creating the string for the search query
             String test = "SELECT * FROM Cost WHERE YEAR(costDate)=" + searchYear;
             ResultSet results = sqlCmd.executeQuery(test);
-
+            // adding the data returned from the database into the arrayList
             while (results.next()){
                 searchCosts.add(new Cost(
                         results.getInt(1),
@@ -333,9 +406,16 @@ public class DerbyConnection {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
+        //returning the arrayList object
         return searchCosts;
     }
 
+    /**
+     * Searches the database and returns an array of all costs that were made in the same year and month as a given parameter.
+     * @param year - parameter of search query, passed through ViewModel.
+     * @param month - parameter of search query, passed through ViewModel.
+     * @return - returns an array of all costs found.
+     */
     public ArrayList<Cost> searchCosts(int year, int month){
         String searchYear = String.valueOf(year);
         String searchMonth = String.valueOf(month);
@@ -367,6 +447,10 @@ public class DerbyConnection {
         return searchCosts;
     }
 
+    /**
+     * Searches for and removes a cost based on its ID from the database
+     * @param id - id of cost that needs to be removed. passed through ViewModel
+     */
     public void removeCost(int id){
         String query = "DELETE  FROM Cost WHERE Id="+id;
         try{
@@ -379,80 +463,4 @@ public class DerbyConnection {
             e.printStackTrace();
         }
     }
-
-    /*
-        catch (SQLException sqle)
-        {
-            sqle.printStackTrace();
-        }
-    */
-
-            /*
-
-            if(rs.next()){
-                rows = (int)rs.getLong(1);
-                System.out.println("We found "+ rows + " rows!!");
-            }
-            else{
-                System.out.println("No rows found!!!");
-                return null;
-            }
-            String query = "SELECT * FROM Cost";
-            rs = s.executeQuery(query);
-            ResultSetMetaData md = rs.getMetaData();
-            int cols = 6;
-            String[][] data = new String[rows][cols];
-            for(int r = 0; r < rows; r++){
-                for(int c = 0; c < cols; c++){
-                    data[r][c] = rs.getString(c);
-                }
-            }
-            return (Object[][]) data;
-        } catch (SQLException sqle)
-        {
-            sqle.printStackTrace();
-        }
-        System.out.println("oopsie! didn't return :P");
-        return null;
-    }
-
-             */
-
-            /*
-    public void insertCost(Record newRecord){
-        try{
-            psCostInsert = conn.prepareStatement("insert  into " )
-            psCategoryInsert.setString(1, catStr);
-            psCategoryInsert.executeUpdate();
-            System.out.println("Inserted Category");
-        } catch (SQLException sqle)
-        {
-            sqle.printStackTrace();
-//            printSQLException(sqle);
-        }
-    }
-    */
-
-    /*
-    public string[][] get_cost(Statement s)
-    {
-
-
-    }
-    */
-
 }
-
-/*
-    - Create a method to return all Cost
-    - Parse all expenses to 2d string array
-    - Return string array to viewmodel -> insert into the gui
-    - Managing categories - create a layout with 3/4 fields -> catName,
-    - lookup datamodels for tables
-    Add new -> ___________  add
-    ID  Name
-    1   Clothes     Edit       Delete
-
-
-
- */
